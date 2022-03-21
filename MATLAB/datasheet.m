7cbc60ad3b22e84cf0940dd6c1b72dd5dc84ad42
DavideFigundio <dfigundio@gmail.com>
2022-03-21 15:14:36 +0100

    Added variable inductance component This component takes values from the file datasheet.m to represent variation in the inductance due to variations in the magnetic air gap.

--- MATLAB/datasheet.m
+++ MATLAB/datasheet.m
@@ -35,3 +35,6 @@ V_vect = [5,5,5,5,5,5,5];
 I_vect = [0.46,0.66,0.725,0.86,0.925,1.005,1.01];
 delta_vect = [0,1.8,2.4,6.56,9.99,14.04,17.8];
 L_vect=sqrt((V_vect./I_vect).^2 - R^2)./31.4;
+
+%% PARAMETERS %%
+Kp_I = 3000;        %Bandwidth for electrical controller
