b29e5d3c944ad2594d6ed7b8e2300d09b7808eaf
DavideFigundio <dfigundio@gmail.com>
2022-05-31 15:45:58 +0200

    Updated and cleaned LQR simulation scheme

--- MATLAB/LQRgen.m
+++ MATLAB/LQRgen.m
@@ -7,7 +7,9 @@ close all
 %% LOADING AND PREPARATION OF DATA %%
 m = 0.83474;                                         % Mobile element mass
 eqforce = m*9.81;                                    % Equilibrium force
-eqpoint = 7;                    % linearization point
+deltaeq = 7;                    % linearization point
+d0 = 14;
+start = d0;
 
 % Data from FEMM simulations
 load('simulationResults.mat')                     % Loading simulation results
@@ -20,25 +22,32 @@ frictionForce = eqforce*fParam;
 
 %% SIMULATED EQUILIBRIUM VALUES %%
 
-% Simulated take-off
-sim_takeoff = contourc(deltas, I, forceArray, [eqforce+frictionForce, eqforce+frictionForce]);
-sim_to = polyfit(sim_takeoff(1, 2:N_I), sim_takeoff(2, 2:N_I), 1); 
+% Simulated equilibrium
+sim_eq = contourc(deltas, I, forceArray, [eqforce, eqforce]);
+gc = polyfit(sim_eq(1, 2:N_I), sim_eq(2, 2:N_I), 1);
 
-% Simulated fall-down
-sim_falldown = contourc(deltas, I, forceArray, [eqforce-frictionForce, eqforce-frictionForce]);
-sim_fd = polyfit(sim_falldown(1, 2:N_I), sim_falldown(2, 2:N_I), 1);
+mG= gc(1);
+qG = gc(2);
 
-% Gradient estimation
-max = polyval(sim_to, eqpoint);
-min = polyval(sim_fd, eqpoint);
-der = 2*frictionForce/(max-min);
+k1=4*eqforce/(mG^2);
+k2=qG*sqrt(k1/eqforce);
 
-A = [0, 1, 0; 0, 0, 1; 0, 0, 0];
-B = [0; 0; der/m];
-Q = diag([1, 10, 0.1]);
+ieq = mG*deltaeq + qG;
+
+a = -4*k1*ieq^2/m/(2*deltaeq+k2)^3;
+b = 2*k1*ieq/m/(2*deltaeq+k2)^2;
+
+
+A = [0, -1; a, 0;];
+B = [0; b];
+C = [1, 0];
+D = 0;
+sys = ss(A, B, C, D);
+
+Q = diag([10, 0.1, 1]);
 R = 1;
 
-K = lqr(A, B, Q , R);
-save('LQRval', "K")
+K = lqi(sys, Q, R);
+
 
 
