17b23046c2242681accca25e6a29951836a40c7b
DavideFigundio <dfigundio@gmail.com>
2022-06-03 18:43:19 +0200

    Added inductometer measurments to comparison.

--- FEMM/indDataAnalysis.m
+++ FEMM/indDataAnalysis.m
@@ -12,8 +12,13 @@ L_sim = inductanceArray(:, 2);
 % Measures for computation of inductances
 V_vect = [5,5,5,5,5,5,5];                           % Voltages
 I_vect = [0.46,0.66,0.725,0.86,0.925,1.005,1.01];   % Currents
-delta_vect = [0.3,1.8,2.4,6.56,9.99,14.04,17.8]/10; % Air gaps
+delta_vect = [0.25,1.8,2.4,6.56,9.99,14.04,17.8]; % Air gaps
 
+% Directly measured inductances
+L_direct = [0.35, 0.25, 0.165, 0.148, 0.129, 0.114, 0.1086];
+delta_direct = [0.1, 1.5, 3.45, 4.85, 7.45, 12.45, 14.45];
+
+% Other measured parameters
 R = 3.7;                                            % El. resistance
 Lfe = 0.048*2 + 2*(0.093-0.028) + 2*0.028;          % Ferrite length
 N = 860;                                            % No of spires
@@ -35,7 +40,8 @@ for i = 1:n
 end
 
 
-%% PLOTTING %% 
+%% PLOTTING %%
+
 tiledlayout(1, 2)
 
 % Raw simulation output surface plot
@@ -49,9 +55,10 @@ zlabel("Inductance [H]")
 nexttile
 hold on
 grid on
-plot(deltas, L_sim)
-plot(deltas, L_theoretical)
+plot(deltas*10, L_sim)
+plot(deltas*10, L_theoretical)
+scatter(delta_direct, L_direct, 'r')
 scatter(delta_vect, L_vect, 'k')
-xlabel('Air Gap [cm]')
+xlabel('Air Gap [mm]')
 ylabel('Inductance [H]')
-legend('FEMM Simulation', 'Magnetic Circuit Model', 'Measured Values')
+legend('FEMM Simulation', 'Magnetic Circuit Model', 'Inductometer Values', 'Reactance Values')
