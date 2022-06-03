5646f68a3bc0b0d05355c9ac6ccd0d19b09fb172
DavideFigundio <dfigundio@gmail.com>
2022-06-03 12:26:24 +0200

    Added plotting of estimated force Improved commenting

--- MATLAB/LQRsetup.m
+++ MATLAB/LQRsetup.m
@@ -1,10 +1,14 @@
-%% LQR Generation %%
+%% LQR SETUP %%
+% Script for the preparation of LQR simulation and the computation of the
+% LQR gain.
 
 clear
 clc
 close all
 
-%% LOADING AND PREPARATION OF DATA %%
+%% PREPARATION OF DATA %%
+
+% System parameters
 m = 0.83474;                    % Mobile element mass
 eqforce = m*9.81;               % Equilibrium force
 deltaeq = 7;                    % linearization point
@@ -18,17 +22,35 @@ deltas = 10*deltas;                     % cm->mm conversion
 N_delta = length(deltas);
 N_I = length(I);
 
-%% SIMULATED EQUILIBRIUM VALUES %%
+%% PARAMETER ESTIMATION %%
 
-% Simulated equilibrium
+% Simulated gravity compensation line
 sim_eq = contourc(deltas, I, forceArray, [eqforce, eqforce]);
 gc = polyfit(sim_eq(1, 2:N_I), sim_eq(2, 2:N_I), 1);
 
-mG= gc(1);
-qG = gc(2);
+%mG= gc(1);
+%qG = gc(2);
 
+% Parameter computation
 k1=4*eqforce/(mG^2);
 k2=qG*sqrt(k1/eqforce);
+
+% Plotting
+formulaForceArray = zeros(N_I, N_delta);
+for i=1:N_delta
+    for j = 1:N_I
+        formulaForceArray(j, i) = k1*I(j)^2/(k2+2*deltas(i))^2;
+    end
+end
+
+surf(deltas, I, formulaForceArray./forceArray)
+xlabel('Air Gap [mm]')
+ylabel('Current [A]')
+zlabel('Ratio')
+
+
+
+%% LINEARIZATION AND LQR %%
 
 ieq = mG*deltaeq + qG;
 
@@ -46,6 +68,7 @@ Q = diag([10, 0.1, 10]);
 R = 1;
 
 K = lqi(sys, Q, R);
+
 
 
 
